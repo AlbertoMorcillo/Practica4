bdc2e1eceaa5df0a22aa572e99d2af10ef463b8d
AlbeX <a.morcillo@sapalomera.cat>
2023-10-16 17:51:33 +0200

    si usuario registrado correctamente mandarlo a vista

--- controlador/signin.php
+++ controlador/signin.php
@@ -1,42 +1,46 @@
 <?php
-//Created by: Alberto Morcillo
+// Created by: Alberto Morcillo
 
-$errors ='';
+$errors = '';
 $insertadoCorrectamente = '';
 
 $validEmail = isset($_POST['email']) ? htmlspecialchars($_POST['email']) : '';
 $validPassword = isset($_POST['password']) ? htmlspecialchars($_POST['password']) : '';
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST'){
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     include_once './validaciones.php';
 
     validarEmailSignin($validEmail, $errors);
     validarPasswordSignin($validPassword, $errors);
 
-    if (empty($errors)){
+    if (empty($errors)) {
         include_once '../modelo/Conection.php';
-        
-        if(validarEmailExistente($validEmail, $connexio)){
-            $errors .= "Ya estas registrado";
+
+        if (validarEmailExistente($validEmail, $connexio)) {
+            $errors .= "Ya estás registrado. Por favor, inicia sesión.";
             header("Location: ../vista/signin_view.php?error=$errors");
             exit();
         } else {
-            $insertadoCorrectamente = insertarUsuario($validEmail, $validPassword)
+            $insertadoCorrectamente = insertarUsuario($validEmail, $validPassword, $connexio);
 
+            if ($insertadoCorrectamente) {
+                header("Location: ../vista/index_view_usuario_logged.php");
+                exit();
+            } else {
+                $errors .= "Hubo un error al registrar el usuario. Por favor, intenta nuevamente.";
+                header("Location: ../vista/signin_view.php?error=$errors");
+                exit();
+            }
         }
     }
 }
 
-
-
-
-
-function entrarEnSignIn(){
+function entrarEnSignIn() {
     if (isset($_POST['signin'])) {
         header('Location: ../vista/signin_view.php');
         exit();
-    } 
+    }
 }
 
-include_once '../vista/signin_view.php'
+include_once '../vista/signin_view.php';
 ?>
