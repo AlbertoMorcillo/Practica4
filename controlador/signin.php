f4de863122228da17fe59b345f983ea3cc75f380
AlbeX <a.morcillo@sapalomera.cat>
2023-10-18 11:16:55 +0200

    Añadido session start en ambos archivos

--- controlador/signin.php
+++ controlador/signin.php
@@ -2,12 +2,11 @@
 // Created by: Alberto Morcillo
 
 $errors = '';
-$insertadoCorrectamente = '';
+$insertadoCorrectamente = false;
 
 $validEmail = isset($_POST['email']) ? htmlspecialchars($_POST['email']) : '';
 $validPassword = isset($_POST['password']) ? htmlspecialchars($_POST['password']) : '';
 $validPasswordRepetida = isset($_POST['passwordRepetida']) ? htmlspecialchars($_POST['passwordRepetida']) : '';
-
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     include_once './validaciones.php';
@@ -21,24 +20,25 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
         if (validarEmailExistente($validEmail, $connexio)) {
             $errors .= "Ya estás registrado. Por favor, inicia sesión.";
-            header("Location: ../vista/signin_view.php?error=$errors");
-            exit();
         } else {
             $insertadoCorrectamente = insertarUsuario($validEmail, $validPassword, $connexio);
-            //ToDo: recuerda que el insertarusuario tiene que devolver un booleano y si es correcto o no hara una de las dos cosas al reves.
+        }
 
-            if ($insertadoCorrectamente) {
-                header("Location: ../vista/index_view_usuario_logged.php");
-                exit();
-            } else {
-                $errors .= "Hubo un error al registrar el usuario. Por favor, intenta nuevamente.";
-                header("Location: ../vista/signin_view.php?error=$errors");
-                exit();
-            }
+        if ($insertadoCorrectamente) {
+            // Iniciar sesión
+            session_start();
+            
+            // Guardar el email del usuario en la sesión
+            $_SESSION['email'] = $validEmail;
+            
+            // Redirigir al usuario a la página de inicio después de iniciar sesión
+            header("Location: ../vista/index_view_usuario_logged.php");
+            exit();
+        } else {
+            $errors .= "Hubo un error al registrar el usuario. Por favor, intenta nuevamente.";
         }
     }
 }
-
 
 include_once '../vista/signin_view.php';
 ?>
---
+++ vista/index_view_usuario_logged.php
@@ -0,0 +1,12 @@
+<?php
+// Iniciar la sesión
+session_start();
+
+// Verificar si el usuario está logueado
+if (!isset($_SESSION['email'])) {
+    // Si el usuario no está logueado, redirigirlo a la página de inicio de sesión
+    header("Location: ../vista/login_view.php");
+    exit();
+}
+
+?>
